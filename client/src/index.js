10d1cd155d8a71454fdca966a7b366952c07d936
chris008123 <aidoochris0081@gmail.com>
2026-04-23 05:35:13 +0000

    Fix API base URL for production

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
+import axios from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
